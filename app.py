13c3a9e505fd43065bf1fa4c24088639a5de8c2a
dellaretiarthur <arthurdellareti@icloud.com>
2023-06-18 17:48:55 -0300

    comentarios adicionais

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+# meu programa inicia aqui 
+
 name = input("Digite o seu nome: ")
 age = input("Digite a sua idade: ")
 
 
 
 print(f"Bem vindo; {name.capitalize()} e você tem {age} anos")
+
+#fim do programa
