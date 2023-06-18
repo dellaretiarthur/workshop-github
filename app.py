065d913bf11ca6f85d1176a003a08789af6ff9a3
dellaretiarthur <arthurdellareti@icloud.com>
2023-06-18 17:37:46 -0300

    tirei a opção de altura porque nao era preciso

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 name = input("Digite o seu nome: ")
 age = input("Digite a sua idade: ")
-altura = float(input("Qual a sua altura: "))
+
 
 
 print(f"Bem vindo; {name.capitalize()} e você tem {age} anos")
